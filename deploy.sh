671609024e262373061d2bf446000f63b53b723e
Aon-Sao <larsatracy@gmail.com>
2024-08-19 01:26:03 -0600

    remove __pycache__s before pushing files

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,7 @@ if test "server" != ""(basename $PWD)""
   exit
 end
 
+# Remove __pycache__ dirs since they're not ingored properly.
+find . -type d -name "__pycache__" -print | grep -v .venv | xargs rm -rf
+
 rsync -avz -e "ssh -i $key" --progress --filter=':- .gitignore' ./* ubuntu@$host:/home/ubuntu/server/
