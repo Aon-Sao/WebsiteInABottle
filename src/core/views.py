b7dfb10a79e961d423217d8c9eb958ba0d6dc27c
Aon-Sao <larsatracy@gmail.com>
2024-08-30 17:49:19 -0600

    serve text from a file in a folder

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,4 @@
+import os
 from flask import Blueprint, render_template
 from flask_login import login_required
 
@@ -7,4 +8,11 @@ core_bp = Blueprint("core", __name__)
 @core_bp.route("/")
 @login_required
 def home():
-    return render_template("core/index.html")
+    entries = []
+    # I should probably add some checks. I could also just let caddy serve static files.
+    for file in os.listdir("content"):
+        with open(f"content/{file}") as fin:
+            entries.append(fin.read())
+    rendered = render_template("core/index.html", entries=entries)
+    print(rendered)
+    return rendered
